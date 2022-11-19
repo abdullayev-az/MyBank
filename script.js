8594f43fb33a09ba838e4b5aca51481e573b08b7
Abdullayev.net <96205201+abdullayev-az@users.noreply.github.com>
2022-11-19 16:26:12 +0400

    element
    initial commit

--- script.js
+++ script.js
@@ -21,27 +21,4 @@ var cards = document.querySelectorAll('.card');
   });
 });
 
-/* $('.home').click(function(){
-  $(this).css({'box-shadow':'inset 0 5px 10px rgba(0, 0, 0, 0.3)'});
-  $('.setting').css({'box-shadow':'none'});
-  $('.more').css({'box-shadow':'none'});
-  $('.more p').text('');
-  $('.setting p').text('');
-  $('.home p').text('Home');
-})
-$('.more').click(function(){
-  $(this).css({'box-shadow':'inset 0 5px 10px rgba(0, 0, 0, 0.3)'});
-  $('.more p').text('More');
-  $('.home').css({'box-shadow':'none'});
-  $('.home p').text('');
-  $('.setting p').text('');
-  $('.setting').css({'box-shadow':'none'});
-})
-$('.setting').click(function(){
-  $(this).css({'box-shadow':'inset 0 5px 10px rgba(0, 0, 0, 0.3)'});
-  $('.home').css({'box-shadow':'none'});
-  $('.more').css({'box-shadow':'none'});
-  $('.more p').text('');
-  $('.home p').text('');
-  $('.setting p').text('Setting');
-}) */
+
